b2cee90c74b74c9260b968c82669d19e86aec15b
Jherson Trigoso <jherson.trigoso@asysteccr.com>
2023-12-08 09:37:47 -0600

    Agregar variable al .env para email de recepcion

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -144,7 +144,7 @@ class ApiController extends Controller
     }
 
     public function sendEmail(Request $request){
-        Mail::to(env('EMAIL_CONTACTs'))->send(new ContactForm($request->all()));
+        Mail::to(env('EMAIL_CONTACT'))->send(new ContactForm($request->all()));
         return back()->with('mensaje','Se envió correctamente');
      }
 }
