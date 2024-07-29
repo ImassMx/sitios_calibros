e2f4a5902455367cdfe78e48fb99d497cf9fabe7
Karina Miranda Mendoza <karina.mendoza@789.mx>
2024-07-29 14:22:07 -0600

    login admin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,8 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Libros Online</title>
     <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
 
 </head>
@@ -14,32 +16,50 @@
 <body>
 
     <main class="login">
-        @if (session('mensaje'))
-            <div class="alert alert-success" role="alert">
-                <p class="error">{{ session('mensaje') }}</p>
-            </div>
-        @endif
-        <div class="contenido">
-            <img src="{{asset('img/logo.jpg')}}" alt="">
-            <div class="body-login">
-                <form action="{{ route('login') }}" method="POST">
-                    @csrf
-                    <div class="inputs">
-                        <label for="">Usuario</label>
-                        <input type="text" placeholder="Ingresa tu usuario" name="email" value="{{old('email') }}">
-                        @error('email')
-                            <p class="error">{{ $message }}</p>
-                        @enderror
+        <div class="container pt-md-5">
+            <div class="row justify-content-center">
+                <div class="col-md-5 col-sm-12 m-md-4">
+                    <div class="card p-3 rounded" style="box-shadow: 0px 5px 11px 3px rgba(186,182,186,1);">
+
+                        <form action="{{ route('login') }}" method="POST">
+                            @csrf
+                            <div class="form-row justify-content-center pt-3">
+                                <div class="form-group col-md-8 col-sm-12 text-center">
+                                    <img src="{{asset('img/logo.jpg')}}" alt="Logo" class="img-fluid mb-4" width="250">
+                                    @if (session('mensaje'))
+                                    <div class="alert alert-success" role="alert">
+                                        <p class="error">{{ session('mensaje') }}</p>
+                                    </div>
+                                    @endif
+                                </div>
+                                <div class="form-group col-md-8 col-sm-12 text-left">
+                                    <div class="inputs">
+                                        <label for="">Usuario</label>
+                                        <input class="form-control" type="text" placeholder="Ingresa tu usuario"
+                                            name="email" value="{{old('email') }}">
+                                        @error('email')
+                                        <p class="error">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="form-group col-md-8 col-sm-12 text-left">
+                                    <div class="inputs">
+                                        <label for="">Contraseña</label>
+                                        <input type="password" placeholder="Ingresa tu contraseña" name="password"
+                                            class="form-control">
+                                        @error('password')
+                                        <p class="error">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="form-group col-md-8 col-sm-12 pt-2 text-center">
+                                    <input type="submit" value="Ingresar" class="ingresar"
+                                        class="btn btn-success w-100 mt-3 mb-2">
+                                </div>
+
+                        </form>
                     </div>
-                    <div class="inputs">
-                        <label for="">Contraseña</label>
-                        <input type="password" placeholder="Ingresa tu contraseña" name="password">
-                        @error('password')
-                            <p class="error">{{ $message }}</p>
-                        @enderror
-                    </div>
-                    <input type="submit" value="INGRESAR" class="ingresar">
-                </form>
+                </div>
             </div>
         </div>
     </main>
