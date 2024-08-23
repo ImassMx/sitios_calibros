bf33f570a7eb470773238f0c60fd2990675674ac
Jherson Trigoso <jherson.trigoso@asysteccr.com>
2024-08-23 14:01:35 -0600

    Solucionar observaciones ultimo

--- resources/js/components/marketplace/ShowComprar.vue
+++ resources/js/components/marketplace/ShowComprar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="card mb-3 p-3 text-center">
+    <div class="card mb-3 p-3 text-center" v-if="isBuy !== true">
         <h5>Adquiere <span class="fw-bold color-p">{{ name }}</span></h5>
         <button @click="comprarBook()" class="btn  btn-sm text-center backg-p text-white"> Comprar ahora</button>
         <button @click="addToBook()" class="btn btn-outline-success btn-sm text-center "><img src="/img/shop.svg" alt="buscar" width="20">
@@ -10,6 +10,15 @@
 <script>
 export default {
         props:["name","uuid","user"],
+        data(){
+            return{
+                isBuy:true
+            }
+        },
+        async created(){
+            const {data} = await axios.get('/api/validate/buy/book',{params: {book : this.uuid,user : this.user}})
+            this.isBuy = data
+        },  
         methods:{
             addToBook() {
             this.$store.dispatch('addToCart', { uuid: this.uuid, user: this.user })
